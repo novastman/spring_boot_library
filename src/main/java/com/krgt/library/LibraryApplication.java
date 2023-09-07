74d9b94284bf687a3f27172c65aa19f489fbd90c
novastman <novastar.swdev@gmail.com>
2023-09-07 11:23:56 -0700

    add componentscan, entityscan and so on.

--- src/main/java/com/krgt/library/LibraryApplication.java
+++ src/main/java/com/krgt/library/LibraryApplication.java
@@ -2,12 +2,17 @@ package com.krgt.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.krgt.library.controller")
+@EntityScan("com.krgt.library.model")
+@EnableJpaRepositories("com.krgt.library.repository")
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
-
 }
