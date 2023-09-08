e4830e945c059f396b633ed4db943192a8c13553
novastman <novastar.swdev@gmail.com>
2023-09-08 05:32:07 -0700

    Add tiles configuration

---
+++ src/main/java/com/krgt/library/TilesConfiguration.java
@@ -0,0 +1,15 @@
+package com.krgt.library;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class TilesConfiguration {
+	
+	@Bean
+	public TilesConfigure()
+}
